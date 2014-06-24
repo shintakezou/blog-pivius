3663911ffd5ba338951fb8f6af175117959acbd7
shintakezou <shintakezou@gmail.com>
2014-06-24 23:28:31 +0200

    gen.cpp: 	generates "nodes" and "edges" according to params 	and hardencoded distributions; illustrates "knowledge" 	as interconnections between "facts" true with a certain 	degree of confidence gen_svg.pl: 	a possible driver which outputs a simple svg from what 	is generated by gen.cpp

---
+++ gen.cpp
@@ -0,0 +1,103 @@
+// g++ -std=c++11 -O3
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <cmath>
+#include <random>
+
+
+
+inline bool is_inside(double r, 
+		      double cx,
+		      double cy,
+		      double x,
+		      double y)
+{
+  return ((x-cx)*(x-cx) + (y-cy)*(y-cy)) < r*r;
+}
+
+const double radius = 100.0;
+const size_t num_dots = 800;
+const int max_connect = 3;
+const double px = 40.0;
+const double py = 40.0;
+const double hole_radius = 40.0;
+const double min_rad = 0.5;
+const double max_rad = 4.0;
+const double relevance_threshold = 0.1;
+const double PI = 3.14159265358979323846;
+
+
+struct node
+{
+  double x;
+  double y;
+  double r;
+  double imp;
+  node(double _r, double _x, double _y, double _imp) :
+    x(_x), y(_y), r(_r), imp(_imp) { }
+  std::string rappr() {
+    std::ostringstream ss;
+    ss << x << " " << y << " " << r << " " << imp;
+    return ss.str(); 
+  }
+};
+
+
+int main()
+{
+  std::default_random_engine rd;
+  std::uniform_real_distribution<double> pos(0, radius);
+  std::uniform_real_distribution<double> ang(0, 2.0*PI);
+  std::uniform_real_distribution<double> sz(min_rad, max_rad);
+  std::uniform_real_distribution<double> prob(0,1.0);
+  
+  std::vector<node> node_list;
+
+  std::cout << "data " << radius << " "
+	    << px << " "
+	    << py << " "
+	    << hole_radius << "\n";
+
+  for (size_t i = 0; i < num_dots; ) {
+    double pr = pos(rd);
+    double theta = ang(rd);
+    double x = pr * cos(theta);
+    double y = pr * sin(theta);
+    double r = sz(rd);
+    double p0 = prob(rd);
+    double imp = p0 > relevance_threshold ? 1.0 + 2.0*p0 : 0.0;
+    if (is_inside(hole_radius, px, py, x, y)) {
+      imp = 0.0;
+      r /= 2.0;
+    }
+    node_list.push_back(node(r, x, y, imp));
+    ++i;
+  }
+
+  std::uniform_int_distribution<size_t> connect(0, node_list.size());
+  std::poisson_distribution<size_t> numcon(max_connect);
+
+  int node_id = 0;
+  for (auto n : node_list) {
+    std::cout << "node " << node_id << " " 
+	      << n.rappr() << "\n";
+    ++node_id;
+  }
+
+  node_id = 0;
+  for (auto n : node_list) {
+    size_t conn_i = numcon(rd);
+    for (size_t i = 0; i < conn_i; ++i) {
+      size_t ni = connect(rd);
+      double conn_strength = n.imp * node_list[ni].imp / 9.0;
+      std::cout << "connect " << node_id << " "
+		<< ni << " " << conn_strength << "\n"; 
+    }
+    ++node_id;
+  }
+
+  return 0;
+}
---
+++ gen_svg.pl
@@ -0,0 +1,63 @@
+#! /usr/bin/perl
+use strict;
+
+print <<EOT;
+<?xml version="1.0"?>
+    <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
+    "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1"
+         width="800px"
+         height="800px"
+         viewBox="-120 -120 240 240">
+EOT
+;
+
+my %node;
+my @connect;
+my $area = "";
+
+while(<>) {
+    if (/^node/) {
+	my ($n, $id, $x, $y, $r, $l) = split /\s+/;
+	$node{$id} = "$x $y $r $l";
+    } elsif (/^connect/) {
+	my ($n, $id0, $id1, $s) = split /\s+/;
+	push @connect, "$id0 $id1 $s";
+    } elsif (/^data/) {
+	my ($rm, $fk, $px, $py, $pr) = split /\s+/;
+	$area = "$px $py $pr";
+    }
+}
+
+# connection first...
+foreach my $c (@connect) {
+    my ($id0, $id1, $s) = split /\s+/, $c;
+    next if ($s*1.0) < 0.1;
+    my ($x1, $y1, $r1, $l1) = split /\s+/, $node{$id0};
+    my ($x2, $y2, $r2, $l2) = split /\s+/, $node{$id1};
+    my $col = "#bababa";
+    if (($s*1.0) > 0.7) {
+	$col = "black";
+    }
+    print "<line x1=\"$x1\" y1=\"$y1\" x2=\"$x2\" y2=\"$y2\" fill=\"none\" stroke=\"$col\" stroke-width=\"0.2\" />";
+}
+
+# then node
+foreach my $n (keys %node) {
+    my ($x, $y, $r, $l) = split /\s+/, $node{$n};
+    my $col = "#C0C0C0";
+    my $op = 0.5;
+    if ($l > 0) {
+	$col = "black";
+	$op = 1.0;
+    }
+    print "<circle cx=\"$x\" cy=\"$y\" r=\"$r\" " .
+	"stroke=\"none\" fill=\"$col\" fill-opacity=\"$op\" />\n";
+}
+
+#my ($px, $py, $pr) = split /\s+/, $area;
+#print "<circle cx=\"$px\" cy=\"$py\" r=\"$pr\" stroke=\"none\" fill=\"blue\" fill-opacity=\"0.2\" />";
+
+print "</svg>\n";
+
+exit 0;
